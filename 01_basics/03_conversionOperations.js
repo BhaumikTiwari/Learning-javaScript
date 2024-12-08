a87a22b9a37fddb6ffba2a9ccde62f4240231163
Bhaumik Tiwari <97089417+BhaumikTiwari@users.noreply.github.com>
2024-12-08 14:34:10 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,23 @@
+let score = 65
+console.log(typeof(score));
+
+let valueInNumber=Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+let a="56abc";
+a = Number(a);
+console.log(a);
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn); 
+
+// 1=> true; 0=> false
+// "" => false
+// "bhaumik" => true
+
+let num=33;
+let str=String(num);
+console.log(str);
+console.log(typeof(str));
