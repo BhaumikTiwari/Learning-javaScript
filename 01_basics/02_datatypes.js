5f6ca58a65488b7e676a005b1fea43c7ded7fd86
Bhaumik Tiwari <97089417+BhaumikTiwari@users.noreply.github.com>
2024-12-08 14:06:33 +0000

    Data Types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+4) // we are using nodejs, not browser
+
+let name = "bhaumik";
+let age = 24;
+let isLoggedIn= false;
+
+//number => 2**53
+//bigint
+//string
+//boolean
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+// object
+
+console.log(typeof "bhaumik"); 
