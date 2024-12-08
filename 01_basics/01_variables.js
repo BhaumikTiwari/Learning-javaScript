3d72fe1ca4858fd791272f1fa6d50c0aeb391594
Bhaumik Tiwari <97089417+BhaumikTiwari@users.noreply.github.com>
2024-12-08 13:37:29 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 2809
+let accEmail = "bhaumik@gmail.com"
+var password = "1234"
+accountCity="Delhi"
+console.log(accountId)
+
+console.table([accountId,accEmail,password,accountCity])
