e15ceea2c726acd246c4933447a144d189763c67
Bhaumik Tiwari <97089417+BhaumikTiwari@users.noreply.github.com>
2024-12-08 15:24:53 +0000

    datatype summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,27 @@
+// Primitive 
+// 7 types : String, Number, Boolean, Null, undefined, Symbol, BigInt
+
+
+const score=100;
+const scoreValue=100.33;
+
+const isLoggedin=false;
+const outsideTemp=null;
+let usermail;
+
+
+// Refrence (non primitive)
+// Arrays, Objects, Functions
+
+const heros=["batman","superman","spiderman"];
+let obj={
+    name:"bhaumik",
+    age:24
+}
+
+const fun = function(){
+    console.log("hello there");
+}
+
+console.log(typeof(obj));
+console.log(typeof(fun));
