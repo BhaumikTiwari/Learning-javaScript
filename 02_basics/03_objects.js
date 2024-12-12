b2c8bb1b604d0ec25f1fa537ca2bd8df5f3ae3f9
Bhaumik Tiwari <97089417+BhaumikTiwari@users.noreply.github.com>
2024-12-12 18:06:52 +0000

    Objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,34 @@
+// singleton
+
+// object literals
+
+const mySym= Symbol("key1");
+console.log(mySym);
+
+const user={
+    name: "Bhaumik",
+    "full name": "Bhaumik Tiwari",
+    [mySym] : "myKey1", // inserting Symbol inside an object
+    age: 25,
+    location: "Kanpur",
+    email: "tiwaribhaumik900@gmail.com",
+    isLogged: false,
+    lastLoginDay: ["Monday","Saturday"]
+}
+
+console.log(user.email);
+console.log(user["email"]); // preferred way
+console.log(user["location"]);
+console.log(user.location);
+console.log(user["full name"]);
+console.log(user[mySym]);
+
+user.email = "tiwaribhaumik5@gmail.com";  // way to override values
+Object.freeze(user); // we are locking the object here so that initial values do not get changed
+user.email = "bhaumik@google.com";  // this line will not throw the error, but new value will also not propogate
+
+user.greeting = function(){
+    console.log("Hello user")
+} 
+
+console.log(user.greeting()); 
