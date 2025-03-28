9391d23bee89b99267b166aeebe391ac09f41122
Bhaumik Tiwari <97089417+BhaumikTiwari@users.noreply.github.com>
2025-03-28 12:11:40 +0000

    Scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,29 @@
+function one(){
+    const username="Bhaumik";
+    function two(){
+        const website="youtube";
+        console.log(username);
+    }
+    two();
+}
+one();
+
+if(true){
+    const username="Bhaumik Tiwari";
+    if(username==="Bhaumik Tiwari"){
+        const website=" youtube";
+        console.log(username+website);
+    }
+}
+
+
+function addone(num){
+    return num+1;
+}
+console.log(addone(10));
+
+const addtwo = function(num){
+    return num+2;
+}
+console.log(addtwo(5));
+
